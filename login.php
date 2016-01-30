8aeb5955d77b8762d10e90140685828e6d98cae4
Keith Yung <krvy42gmail.com>
2016-01-30 17:43:00 +0800

    Sample PHP file that allows the user to login to the system, whether he's an admin or student.
    Credits to Jan David Umali.

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Login Page</title>
+</head>
+
+  <form action = "handler.php" method="post"> 
+     <h1>Log in</h1> 
+     <p> 
+          Username:
+          <input id="username" name="username" required="required" type="text" placeholder = "Username"/>
+     </p>
+     <p> 
+          Password:
+          <input id="password" name="password" required="required" type="password" placeholder = "Password"/> 
+     </p>
+     <p> 
+          <input type="submit" value="Login" /> 
+     </p>
+</form>
+    </body>
+
+</html>
