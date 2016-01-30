3cef4fa4555c2b817ee2146325e4f0bb92ccebe3
Keith Yung <krvy42gmail.com>
2016-01-30 17:43:56 +0800

    The user is redirected to this sample file if the user is a student.
    Credits to Jan David Umali.

---
+++ studentWindow.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Student Window</title>
+</head>
+    <body>
+    <p>This is the Student Window.</p>
+    <?php
+
+	?>
+	</body>
+</html>
