33ac8903ff2e62899ac9f4a528484ab90aa2c10e
Keith Yung <krvy42gmail.com>
2016-01-30 14:56:22 +0800

    PHP file that redirects the user to a sample admin window if the user is an Admin.
     Credits to Jan David Umali.

---
+++ adminWindow.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<title>Admin Window</title>
+</head>
+    <body>
+    <p>This is the admin page.</p>
+    <?php
+
+	?>
+    </body>
+
+</html>
