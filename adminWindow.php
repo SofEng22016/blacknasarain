a47afa00238e6d9eb14258ecffc103211a764c6d
Keith Yung <krvy42gmail.com>
2016-02-15 14:34:05 +0800

    Updated handler.php file that can verifies whether the user account is already in the database. Otherwise it will return a message prompting the user that "No such user exists.".

--- adminWindow.php
+++ adminWindow.php
@@ -3,12 +3,23 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
 <title>Admin Window</title>
+<link rel="stylesheet" href="http://bootswatch.com/superhero/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
     <body>
-    <p>This is the admin page.</p>
+    <div class="container">
+    <div class = "jumbotron">
+    <p class = "text-center">This is the admin page.</p>
+    
+	
     <?php
 
 	?>
+	</div>
+	<a href="addRoomsAdmin.php" class="btn btn-success btn-block" role="button">Add Available Rooms</a>
+    <a href="viewPendingRooms.php" class="btn btn-success btn-block" role="button">View Pending Rooms</a>
+	</div> 
     </body>
 
 </html>
