548e949455c08f49cfdba2e334778aa3d1620da8
Keith Yung <krvy42gmail.com>
2016-02-15 14:37:53 +0800

    No initial changes made to handler. Missclick.

--- handler.php
+++ handler.php
@@ -2,7 +2,7 @@
 
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	
+
 	$user = "root";
 	$pass = "";
 	$dbname = "databasePHP";
@@ -22,17 +22,19 @@ if ($result->num_rows > 0) {
 		
     // output data of each row
     while($row = $result->fetch_assoc()) {
-    	if($row["username"] == $username){
+    	if($row["username"] == $username && $row["password"] == $password){
     		if($row["ADMINorSTUDENT"]){
+    			session_start();
+    			$_SESSION['username'] = $username;
     			header('Location: studentWindow.php');
     		} else {
+    			
     			header('Location: adminWindow.php');
     		}
     		
     	} else {
     		echo '<script language="javascript">';
 			echo 'alert("No such user exists.")';
-			echo 'window.location.replace("login.php/")';
 			echo '</script>';
       		
    		} 
