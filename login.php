a61dee8973ac192e7aca523f8a5227e98fe238bb
Keith Yung <krvy42gmail.com>
2016-02-15 14:42:03 +0800

    Updated login.php file now with Bootstrap properties.

--- login.php
+++ login.php
@@ -1,24 +1,47 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=Cp1252">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html">
 <title>Login Page</title>
+<link rel="stylesheet" href="http://bootswatch.com/superhero/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
-
+<body>
+<div class="jumbotron">
+  	<h1 align="center">Room Reservation System</h1>
+</div>
+<div class="container">
+ 
+  <div class="row">
+ 	<div class="col-md-4"></div>
+  	<div class="col-md-4"></div>
+  	<div class="col-md-4"></div>
+ </div>
+ 
   <form action = "handler.php" method="post"> 
-     <h1>Log in</h1> 
+ 	 <div class="row">
+ 	 <div class="col-md-4"></div>
+     <div class="col-md-4">
+     	<h1 align ="center">Log in</h1> 
      <p> 
           Username:
-          <input id="username" name="username" required="required" type="text" placeholder = "Username"/>
+          <input id="username" name="username" required="required" type="text" placeholder = "Username" class="form-control"/>
      </p>
      <p> 
           Password:
-          <input id="password" name="password" required="required" type="password" placeholder = "Password"/> 
+          <input id="password" name="password" required="required" type="password" placeholder = "Password" class="form-control"/> 
      </p>
-     <p> 
-          <input type="submit" value="Login" /> 
+     <p align ="center"> 
+          <input type="submit" value="Login" class="btn btn-success"/> 
      </p>
-</form>
-    </body>
+  </div>
+  <div class="col-md-4"></div>
+  </div>
+  </form>
+</div>
+</body>
 
 </html>
