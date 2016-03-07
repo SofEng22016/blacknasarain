39ec8cc538ff05d24be4b068415c4f505d453632
Keith Yung <krvy42gmail.com>
2016-03-07 14:40:21 +0800

    Added new Bootstrap theme.

--- handler.php
+++ handler.php
@@ -25,7 +25,7 @@ if ($result->num_rows > 0) {
     // output data of each row
 	while($row = $result->fetch_assoc()){
 		if($row["username"] == $username){
-if($row["ADMINorSTUDENT"]){
+		if($row["ADMINorSTUDENT"]){
        session_start();
        $_SESSION['username'] = $username;
        header('Location: studentWindow.php');
--- login.php
+++ login.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html">
 <title>Login Page</title>
-<link rel="stylesheet" href="http://bootswatch.com/superhero/bootstrap.min.css">
+<link rel="stylesheet" href="http://bootswatch.com/simplex/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
 <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
